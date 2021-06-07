ef1271509d6518b10e15221afce03c6f99443d1c
YourEyes <72436392+YourEyesGG@users.noreply.github.com>
2021-06-07 08:56:43 +0700

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+    ['invalid_amount'] = 'Jumlah tidak valid',
+    ['atm_blip'] = 'ATM ',
+    ['bank_blip'] = 'BANK',
+    ['atm_open'] = 'Tekan ~INPUT_PICKUP~ Untuk mengakses Akun ~b~',
+    ['no_money'] = 'Uang mu tidak ada',
+    ['recieved1'] = 'Kamu menerima uang',
+    ['recieved2'] = 'Anda menerimah uang sejumlah',
+    ['recieved3'] = ' Rupiah',
+    ['removed1'] = 'Menerima Pembayaran',
+    ['removed2'] = 'Pembayaran dari',
+    ['removed3'] = 'Rupiah'
+}
